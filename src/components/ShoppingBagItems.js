f237a6721b48825b5a28f4d8ee6cc8a428706a86
Theran <theran.brigowatz@gmail.com>
2020-01-23 09:04:53 +0900

    updated cart with size

--- src/components/ShoppingBagItems.js
+++ src/components/ShoppingBagItems.js
@@ -83,7 +83,7 @@ const ShoppingBagItems = ({ items, cartLoading, canEdit, mode }) => {
                 <div>
                   <h2>{item.brand.toUpperCase()}</h2>
                   <h3>
-                    {item.name.toUpperCase()} - {item.size}
+                    {item.name.toUpperCase()} - {item.selectedSize}
                   </h3>
                 </div>
                 <h4>{`$${(item.price / 100).toFixed(2)}`}</h4>
