86b75125d8f0cd65944ce07e86cebbc54e82d838
Theran <theran.brigowatz@gmail.com>
2020-02-10 17:47:34 +0900

    basic search setup

--- src/components/FirebaseSearch.js
+++ src/components/FirebaseSearch.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { debounce } from 'debounce';
 import { FirebaseContext } from '../context/Firebase';
+import { Link } from 'react-router-dom';
+import { formatProductName } from '../utilities/formatting';
 
 const FirebaseSearch = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -8,21 +10,72 @@ const FirebaseSearch = () => {
 
   const { dbh } = useContext(FirebaseContext);
 
-  const searchForProduct = () => {
-    const snapshot = () =>
-      dbh
+  const searchForProduct = query => {
+    let tempItems = [];
+    const queryDB = async () => {
+      await dbh
         .collection('products')
         .orderBy('name')
-        .where('product', 'array-contains', searchQuery)
+        .startAt(query)
+        .endAt(query + '\uf8ff')
         .get()
-        .then(querySnapshot => console.log(querySnapshot));
-
-    snapshot();
+        .then(querySnapshot => {
+          querySnapshot.docs.forEach(doc => {
+            const { name, price, mainImage, brand } = doc.data();
+            tempItems.push({
+              id: doc.id,
+              name: formatProductName(name),
+              price,
+              mainImage,
+              brand: brand.toUpperCase(),
+            });
+          });
+          setProducts(tempItems);
+        });
+      await dbh
+        .collection('products')
+        .orderBy('brand')
+        .startAt(query)
+        .endAt(query + '\uf8ff')
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.docs.forEach(doc => {
+            const { name, price, mainImage, brand } = doc.data();
+            tempItems.push({
+              id: doc.id,
+              name: formatProductName(name),
+              price,
+              mainImage,
+              brand: brand.toUpperCase(),
+            });
+          });
+          setProducts(tempItems);
+        });
+    };
+    debounce(queryDB(), 500);
   };
 
   return (
     <div>
-      <input type='text' onChange={() => debounce(searchForProduct())} />
+      <input
+        value={searchQuery}
+        onChange={e => {
+          if (e.target.value === '') {
+            setProducts([]);
+          } else {
+            searchForProduct(e.target.value);
+          }
+          setSearchQuery(e.target.value);
+        }}
+        type='text'
+      />
+      <ul>
+        {products.slice(0, 5).map(product => (
+          <div key={product.id}>
+            <Link to={`/product/${product.id}`}>{product.name}</Link>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/context/Product.js
+++ src/context/Product.js
@@ -16,7 +16,7 @@ const ProductProvider = ({ children }) => {
     let curName = '';
     name.split('').forEach(letter => {
       curName += letter;
-      arrName.push(curName);
+      arrName.push(curName.toLowerCase());
     });
     return arrName;
   };
