7268dcf97334e32562122404e6cda0b25d463be6
Theran <theran.brigowatz@gmail.com>
2020-02-18 17:10:59 +0900

    search basic styled changed

--- src/components/FirebaseSearch.js
+++ src/components/FirebaseSearch.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect, useContext } from 'react';
 import debounce from 'lodash.debounce';
 import { FirebaseContext } from '../context/Firebase';
 import { Link, useHistory } from 'react-router-dom';
-import { formatProductName } from '../utilities/formatting';
+import { formatProductName, formatPrice } from '../utilities/formatting';
 import Highlighter from 'react-highlight-words';
 import styled from 'styled-components';
 import { BackButtonStyles } from './BackButton';
+import { BlackLink } from '../utilities/ReusableStyles';
 
 export const SearchStyles = styled.div`
   input {
@@ -21,11 +22,11 @@ export const SearchStyles = styled.div`
     font-family: Montserrat, sans-serif;
     padding: 3px;
   }
-  a {
+  a.search-link {
     background: transparent;
     color: ${props => props.theme.colors.white};
     width: 100%;
-    font-family: Montserrat, sans-serif;
+    font-family: ${props => props.theme.fonts.main};
     font-size: 1.4rem;
     border: none;
     padding: 10px 0;
@@ -39,6 +40,47 @@ export const SearchStyles = styled.div`
       font-weight: 600;
       color: ${props => props.theme.colors.white};
     }
+  }
+  .products-list {
+    background: ${props => props.theme.colors.white};
+    border-bottom: 1px solid ${props => props.theme.colors.grey};
+    a.display-link {
+      display: grid;
+      grid-template-columns: 100px 3fr 1fr;
+      grid-gap: 10px;
+      background: ${props => props.theme.colors.white};
+      text-decoration: none;
+      h3,
+      h4,
+      h5 {
+        color: ${props => props.theme.colors.black};
+        font-family: ${props => props.theme.fonts.main};
+      }
+    }
+  }
+`;
+const SearchLinkStyles = styled.div`
+  background: white;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  height: 10vh;
+  width: 100%;
+  text-align: center;
+  display: grid;
+  grid-template-columns: 1fr;
+  align-items: center;
+  justify-content: center;
+  a {
+    padding: 0px 20px;
+    line-height: 50px;
+    margin: 20px auto;
+    text-align: center;
+    border-radius: 25px;
+    background: ${props => props.theme.colors.black};
+    color: ${props => props.theme.colors.white};
+    text-decoration: none;
+    font-family: ${props => props.theme.fonts.main};
   }
 `;
 
@@ -72,42 +114,68 @@ const FirebaseSearch = ({ setOpenSearch }) => {
   }, 150);
 
   return (
-    <SearchStyles>
-      <input
-        aria-label='search input'
-        value={searchQuery}
-        onChange={e => {
-          if (e.target.value === '') {
-            setProducts([]);
-          } else {
-            searchForProduct(e.target.value);
-          }
-          setSearchQuery(e.target.value);
-        }}
-        type='text'
-      />
-      {products.map(product => {
-        return (
-          <Link
-            key={product.id}
-            name={product.name}
-            id={product.id}
-            onClick={() => {
-              setOpenSearch(false);
-              setSearchQuery('');
+    <>
+      <SearchStyles>
+        <input
+          aria-label='search input'
+          value={searchQuery}
+          onChange={e => {
+            if (e.target.value === '') {
               setProducts([]);
-              history.push(`/product/${product.id}`);
-            }}>
-            <Highlighter
-              highlightClassName='highlighted-text'
-              searchWords={[searchQuery]}
-              autoEscape={true}
-              textToHighlight={`${product.brand} ${product.name}`}
-            />
-          </Link>
-        );
-      })}
-    </SearchStyles>
+            } else {
+              searchForProduct(e.target.value);
+            }
+            setSearchQuery(e.target.value);
+          }}
+          type='text'
+        />
+        {products.map(product => {
+          return (
+            <Link
+              className='search-link'
+              key={product.id}
+              name={product.name}
+              id={product.id}
+              onClick={() => {
+                setOpenSearch(false);
+                setSearchQuery('');
+                setProducts([]);
+                history.push(`/product/${product.id}`);
+              }}>
+              <Highlighter
+                highlightClassName='highlighted-text'
+                searchWords={[searchQuery]}
+                autoEscape={true}
+                textToHighlight={`${product.brand} ${product.name}`}
+              />
+            </Link>
+          );
+        })}
+        <div className='products-list'>
+          {products.map(product => (
+            <Link
+              to={`/product/${product.id}`}
+              className='display-link'
+              onClick={() => {
+                setOpenSearch(false);
+                setSearchQuery('');
+                setProducts([]);
+                history.push(`/product/${product.id}`);
+              }}>
+              <img src={product.mainImage} alt={product.name} height='100px' width='100px' />
+              <div className='link-info'>
+                <h3>{product.brand}</h3>
+                <h4>{product.name}</h4>
+              </div>
+              <h5>{formatPrice(product.price)}</h5>
+            </Link>
+          ))}
+        </div>
+      </SearchStyles>
+      <SearchLinkStyles>
+        <Link>SEE ALL RESULTS ({`${products.length}`})</Link>
+      </SearchLinkStyles>
+    </>
   );
 };
 
--- src/components/SearchModal.js
+++ src/components/SearchModal.js
@@ -1,36 +1,39 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import { useHistory, useParams } from 'react-router-dom';
-import DropdownSearch from './DropdownSearch';
+import { useHistory, useParams, Link } from 'react-router-dom';
 import FirebaseSearch from './FirebaseSearch';
+import { BlackLink } from '../utilities/ReusableStyles';
 
 export const ModalStyles = styled.div`
-  height: ${({ openSearch }) => (openSearch ? '410px' : '0px')};
+  height: ${({ openSearch }) => (openSearch ? '80vh' : '0px')};
   transform: ${({ openSearch }) => (openSearch ? 'scaleY(100%)' : 'scaleY(0)')};
   -webkit-box-shadow: 0px 10px 0px 0px rgba(0, 0, 3, 0.28);
   -moz-box-shadow: 0px 10px 0px 0px rgba(0, 0, 3, 0.28);
   box-shadow: 0px 10px 0px 0px rgba(0, 0, 3, 0.28);
   transition: 0.5s;
-  background: #000000ee;
+  background: ${props => props.theme.colors.black};
   position: fixed;
   top: 10vh;
   width: 100%;
   overflow-y: scroll;
   z-index: 501;
-  .modal-shadow {
-    height: ${({ openSearch }) => (openSearch ? 'calc(90vh - 410px)' : '5px')};
-    background: #23232394;
+  .modal-bottom {
+    height: ${({ openSearch }) => (openSearch ? 'calc(10vh)' : '5px')};
+    background: ${props => props.theme.colors.white};
     position: fixed;
     background-attachment: fixed;
     width: 100vw;
     left: 0;
     bottom: 0;
+    a {
+      width: 200px;
+    }
   }
   .modal-content {
     width: 500px;
     max-width: 95%;
     margin: 0 auto;
-    font-family: Montserrat, sans-serif;
+    font-family: ${props => props.theme.fonts.main};
     h3 {
       color: white;
       font-weight: 300;
@@ -51,7 +54,7 @@ export const ModalStyles = styled.div`
       height: 30px;
       width: 30px;
       text-align: center;
-      font-family: Montserrat, sans-serif;
+      font-family: ${props => props.theme.fonts.main};
       font-size: 1.1rem;
       line-height: 1.6rem;
       background: white;
@@ -84,14 +87,8 @@ const SearchModal = ({ openSearch, setOpenSearch }) => {
               <i className='fa fa-close' aria-hidden='true'></i>
             </button>
           </div>
-          {/* <DropdownSearch setOpenSearch={setOpenSearch} openSearch={openSearch} /> */}
           <FirebaseSearch setOpenSearch={setOpenSearch} openSearch={openSearch} />
         </div>
-        <div
-          className='modal-shadow'
-          onClick={() => {
-            setOpenSearch(false);
-          }}></div>
       </ModalStyles>
     </>
   );
