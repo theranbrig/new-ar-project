39142715e21f0072ee61a43a0b18b523af3d3672
Theran <theran.brigowatz@gmail.com>
2020-04-14 14:31:23 +0900

    location lifecycle

--- src/context/Location.js
+++ src/context/Location.js
@@ -1,21 +1,56 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import PropTypes from 'prop-types';
 
 export const LocationContext = React.createContext();
 
 const LocationProvider = ({ children }) => {
-  const [currentLocation, setCurrentLocation] = useState({ lat: 0, lon: 0 });
+  const [currentLocation, setCurrentLocation] = useState({ lat: 0, long: 0 });
+  const [address, setAddress] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const Geolocation = navigator.geolocation;
-  const location = Geolocation.getCurrentPosition((pos) =>
-    setCurrentLocation({ lat: pos.coords.latitude, long: pos.coords.longitude })
-  );
+  if (currentLocation.lat === 0 && currentLocation.long === 0) {
+    Geolocation.getCurrentPosition((pos) => {
+      const coords = { lat: pos.coords.latitude, long: pos.coords.longitude };
+      setCurrentLocation(coords);
+    });
+  }
+
+  const getAddress = async () => {
+    setLoading(true);
+    if (currentLocation.lat !== 0 && currentLocation.long !== 0) {
+      fetch(
+        `https://us1.locationiq.com/v1/reverse.php?key=${process.env.REACT_APP_location_iq_API_key}&lat=${currentLocation.lat}&lon=${currentLocation.long}&format=json`
+      )
+        .then((res) => {
+          res
+            .json()
+            .then((json) => {
+              setAddress(json.display_name);
+              setLoading(false);
+            })
+            .catch((err) => {
+              setLoading(false);
+            });
+          setLoading(false);
+        })
+        .catch((err) => {
+          setLoading(false);
+        });
+    }
+  };
+
+  useEffect(() => {
+    getAddress();
+  }, [currentLocation]);
 
   return (
     <LocationContext.Provider
       value={{
+        address,
         currentLocation,
+        locationLoading: loading,
       }}>
       {children}
     </LocationContext.Provider>
--- src/pages/Location.js
+++ src/pages/Location.js
@@ -3,14 +3,15 @@ import React, { setState, useContext } from 'react';
 import { LocationContext } from '../context/Location';
 
 export const Location = () => {
-  const { currentLocation } = useContext(LocationContext);
-  console.log(currentLocation);
+  const { currentLocation, address } = useContext(LocationContext);
+
   return (
     <div>
       <h1>Location</h1>
       <p>
         You are located at "{currentLocation.lat}", "{currentLocation.long}"
       </p>
+      <p>Your address is {address}</p>
     </div>
   );
 };
