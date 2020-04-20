c54067c406898a276d853307f8abc04f3c76ece9
Theran <theran.brigowatz@gmail.com>
2020-04-20 11:29:11 +0900

    updated sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,16 @@
 importScripts('https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.14.0/firebase-messaging.js');
 
-
+const firebaseConfig = {
+  apiKey: 'AIzaSyCbeKQnNn5ZzS_6BajkJwlJ5qEF-b3NDF4',
+  authDomain: 'yzed-dev.firebaseapp.com',
+  databaseURL: 'https://yzed-dev.firebaseio.com',
+  projectId: 'yzed-dev',
+  storageBucket: 'yzed-dev.appspot.com',
+  messagingSenderId: '528819968183',
+  appId: '1:528819968183:web:d80c32d7ac8b6b54b01532',
+  measurementId: 'G-RPGT39JQP9',
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
